34eed1036d475e5343cc932f8764e72289924499
esbb48 <esbb48@gmail.com>
2021-01-29 17:03:54 +0800

    Rename file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Navigation } from 'react-native-navigation';
 import { getToken } from '~/utils/asyncStorage';
 import authRoutes from '~/routes/authRoutes';
 import noAuthRoutes from '~/routes/noAuthRoutes';
-import '~/routes';
+import '~/routes/registerScreens';
 
 Navigation.events().registerAppLaunchedListener(async () => {
   const isLogined = await getToken();

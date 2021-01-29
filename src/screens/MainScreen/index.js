b55e3890f889da486c7e32babb208150af6c89d9
esbb48 <esbb48@gmail.com>
2021-01-29 14:21:58 +0800

    Add force eslint format

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements'
+import { ListItem } from 'react-native-elements';
 import { useQuery } from '@apollo/client';
 import { GET_EXCHANGE_RATES } from '~/gql/exchange';
 

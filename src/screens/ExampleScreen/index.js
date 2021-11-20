0ffde52f8cdc2d351205529239e58042f5adde76
Min-Chun Shih <mcshihs@gmail.com>
2021-11-20 14:58:26 +0800

    added divider comparison

--- src/screens/ExampleScreen/index.js
+++ src/screens/ExampleScreen/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
 import Title from '~/components/example/Title';
 import GraphqlResultList from '~/components/example/GraphqlResultList';
+import { Divider, Text } from 'react-native-elements';
+import  MyDivider from '~/components/MyDivider';
+
 
 const ExampleScreen = () => {
   return (
     <ScrollView style={styles.container}>
       <Title>Result List from GraphQL</Title>
       <GraphqlResultList style={styles.graphqlResultList} />
+      <Text>Divider Element</Text>
+      <Divider />
+      <Text>Divider Element</Text>
+      <Divider />
+      <Text>MyDivider</Text>
+      <MyDivider />
+      <Text>MyDivider</Text>
+      <MyDivider />
     </ScrollView>
+
   );
 };
 

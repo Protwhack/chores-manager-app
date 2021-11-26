4e1fa083a1a12751890b4f648c2153394d141113
malikid <malikid.chang@gmail.com>
2021-11-27 00:06:23 +0100

    feat: update graphql on Example screen

--- src/components/example/GraphqlResultList.js
+++ src/components/example/GraphqlResultList.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
 import { useQuery } from '@apollo/client';
-import { GET_AREAS } from '~/gql/areas';
+import { GET_AREAS_CHORES } from '~/gql/areas';
 import { keyExtractor } from '~/utils/list';
 
 const renderItem = ({ item }) => (
   <ListItem bottomDivider>
-    <Avatar source={{ uri: item.imageUrl }} />
+    <Avatar source={{ uri: item.iconUrl }} />
     <ListItem.Content>
       <ListItem.Title>{item.name}</ListItem.Title>
       <ListItem.Subtitle>{`Color: ${item.color}`}</ListItem.Subtitle>
-      <ListItem.Subtitle>{`Note: ${item.note}`}</ListItem.Subtitle>
+      <ListItem.Subtitle>{`Chores: ${item.chores}`}</ListItem.Subtitle>
     </ListItem.Content>
     <ListItem.Chevron />
   </ListItem>
 );
 
 const GraphqlResultList = props => {
-  const { data, loading } = useQuery(GET_AREAS);
+  const { data, loading } = useQuery(GET_AREAS_CHORES);
   if (loading) {
     return (
       <View style={props.style}>
